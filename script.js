d8e280696a4e021a044434f4929bcaa823d6858c
Flores, Alejo Mohamed <floresalejo3@gmail.com>
2022-05-26 19:54:58 -0300

    -
    -

---
+++ script.js
@@ -0,0 +1,6 @@
+        const but = document.getElementById("but");
+        const lin = document.getElementById("lin");
+
+        but.addEventListener("click", (e)=>{
+            lin.classList.toggle("collapsed")
+        });
